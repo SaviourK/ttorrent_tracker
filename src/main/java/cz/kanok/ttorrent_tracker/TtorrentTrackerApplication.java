50cbea6e94a0aa580eefcc0946e6487b09ed6963
Vitezslav Kanok <vitezslav.kanok@sykora.cz>
2020-11-16 21:47:09 +0100

    Initial commit

---
+++ src/main/java/cz/kanok/ttorrent_tracker/TtorrentTrackerApplication.java
@@ -0,0 +1,13 @@
+package cz.kanok.ttorrent_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtorrentTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtorrentTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/kanok/ttorrent_tracker/TtorrentTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.kanok.ttorrent_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtorrentTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
